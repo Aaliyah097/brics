19c5f8d21da806f6f0ae81b9e0a5778108708877
eldenhard <elden.hard@mail.ru>
2025-02-24 21:35:45 +0300

    [FRONTEND]
    -Реализация сворачивания разворачивания блоков, верстка footer

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -6,3 +6,25 @@ Http.send();
 Http.onreadystatechange = (e) => {
   console.log(Http.responseText);  // => получим массив данных в формате JSON
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".team_brics__finance_strange");
+
+  buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+          const column = this.closest(".team_brics__column");
+          column.classList.toggle("open");
+
+          const content = column.querySelector(".container-finance");
+          if (content) {
+              if (column.classList.contains("open")) {
+                  content.style.maxHeight = content.scrollHeight + "px";
+                  content.style.opacity = "1";
+              } else {
+                  content.style.maxHeight = "0";
+                  content.style.opacity = "0";
+              }
+          }
+      });
+  });
+});
